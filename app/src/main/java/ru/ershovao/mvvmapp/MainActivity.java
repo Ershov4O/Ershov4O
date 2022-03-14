11f0763142c07f7bd059f103e540ad24e268cc6e
Ershov4O <95ersh@mail.ru>
2022-03-14 20:02:14 +1000

    Initial commit

---
+++ app/src/main/java/ru/ershovao/mvvmapp/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.ershovao.mvvmapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
